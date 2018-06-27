1e51b2fd2ab9553c919debfe46aed7fc8c24e8cc
emanuelbierman <emanuel.bierman@gmail.com>
2018-06-27 12:11:02 -0400

    Delete module namespace in environment file

--- lib/free_today_nyc_parks.rb
+++ lib/free_today_nyc_parks.rb
@@ -1,8 +1,4 @@
 require_relative "./free_today_nyc_parks/version"
-
-module FreeTodayNycParks
-end
-
 require_relative "free_today_nyc_parks/cli"
 require_relative "free_today_nyc_parks/event"
 require_relative "free_today_nyc_parks/scraper"
--- lib/free_today_nyc_parks/cli.rb
+++ lib/free_today_nyc_parks/cli.rb
@@ -10,22 +10,22 @@ class FreeTodayNycParks::CLI
     puts "Every day there are free events happening all over NYC’s public parks."
     puts ""
     puts "Here are the events happening today:"
-    FreeTodayNycParks::Scraper.new.scrape
-    FreeTodayNycParks::Event.print_list
-    puts "If you want to refine your search by borough, type in the borough:"
-    puts "If you only want to see results starting from a certain time today, type in that time:"
-    puts "To repeat the list of events happening today, type 'list':"
-    puts "Otherwise, type 'exit':"
-    input = gets.strip.downcase
-    while input != "exit"
-      if input == "list"
-        FreeTodayNycParks::Event.print_list
-      elsif input.to_i.between?(0,24)
-        FreeTodayNycParks::Event.filter_date(input)
-      elsif @@boroughs.include?(input)
-        FreeTodayNycParks::Event.all.filter_borough(input)
-      end
-    end
+    # FreeTodayNycParks::Scraper.new.scrape
+    # FreeTodayNycParks::Event.print_list
+    # puts "If you want to refine your search by borough, type in the borough:"
+    # puts "If you only want to see results starting from a certain time today, type in that time:"
+    # puts "To repeat the list of events happening today, type 'list':"
+    # puts "Otherwise, type 'exit':"
+    # input = gets.strip.downcase
+    # while input != "exit"
+    #   if input == "list"
+    #     FreeTodayNycParks::Event.print_list
+    #   elsif input.to_i.between?(0,24)
+    #     FreeTodayNycParks::Event.filter_date(input)
+    #   elsif @@boroughs.include?(input)
+    #     FreeTodayNycParks::Event.all.filter_borough(input)
+    #   end
+    # end
   end
 
 end
