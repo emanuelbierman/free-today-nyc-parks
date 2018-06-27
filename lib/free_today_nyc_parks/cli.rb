cb7c3fc399b80e372b4c79ebb0d3982c892973af
emanuelbierman <emanuel.bierman@gmail.com>
2018-06-27 11:49:46 -0400

    Add basic cli methods. Add initial cli call in bin folder

--- lib/free_today_nyc_parks/cli.rb
+++ lib/free_today_nyc_parks/cli.rb
@@ -1,3 +1,32 @@
 class FreeTodayNycParks::CLI
 
+  @@boroughs = [manhattan, brooklyn, bronx, queens, staten island]
+
+  def call
+    start
+  end
+
+  def start
+    puts "Welcome to Free Today in NYC!"
+    puts "Every day there are free events happening all over NYC’s public parks."
+    puts ""
+    puts "Here are the events happening today:"
+    Free_NYC::Scraper.new.scrape
+    Free_NYC::Event.print_list
+    puts "If you want to refine your search by borough, type in the borough:"
+    puts "If you only want to see results starting from a certain time today, type in that time:"
+    puts "To repeat the list of events happening today, type 'list':"
+    puts "Otherwise, type 'exit':"
+    input = gets.strip.downcase
+    while input != "exit"
+      if input == "list"
+        Free_NYC::Event.print_list
+      elsif input.to_i.between?(0,24)
+        Free_NYC::Event.filter_date(input)
+      elsif @@boroughs.include?(input)
+        Free_NYC::Event.all.filter_borough(input)
+      end
+    end
+  end
+
 end
