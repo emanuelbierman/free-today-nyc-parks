31fbc13f64022d55e601716ff2517b24de8be128
emanuelbierman <emanuel.bierman@gmail.com>
2018-06-27 12:26:05 -0400

    Complete #print_list stub between CLI and Events classes

--- lib/free_today_nyc_parks/event.rb
+++ lib/free_today_nyc_parks/event.rb
@@ -1,3 +1,30 @@
 class FreeTodayNycParks::Event
+  attr_accessor :title, :borough, :location, :time, :description, :url
 
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def self.print_list
+    puts "Here's a formatted list!"
+    # use heredocs to format this list?
+    # self.all.each do |event|
+      # puts event.title
+      # puts event.location
+      # puts event.time
+      # puts event.description
+      # puts event.url
+    # end
+  end
+
+  def self.filter_borough(borough)
+    puts "Here are today's free events in #{borough}:"
+  end
+
+  def self.filter_time(time)
+    puts "Here are today's free events starting from #{time}"
+  end
 end
