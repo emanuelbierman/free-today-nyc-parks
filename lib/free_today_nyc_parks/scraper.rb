2b982a93ee2238d648226aaeb55bdb9bb7942f12
emanuelbierman <emanuel.bierman@gmail.com>
2018-06-29 10:32:48 -0400

    Scrape unformatted locations into locations array

--- lib/free_today_nyc_parks/scraper.rb
+++ lib/free_today_nyc_parks/scraper.rb
@@ -8,8 +8,17 @@ class FreeTodayNycParks::Scraper
       doc.css("div#events_leftcol h3 a").each do |title|
         FreeTodayNycParks::Event.new(title.content)
       end
+
       today = doc.css("div#events_leftcol h2").text
       FreeTodayNycParks::Event.today=(today)
+
+      array_of_locations = @doc.css("div#events_leftcol div h4.location span").text.split(", ")
+
+      locations = []
+      doc.css("div#events_leftcol div h4.location span").each {|e| locations << e.text }
+
+      one_location = doc.css("div#events_leftcol div h4.location span")[0].text
+
   end
 
 end
