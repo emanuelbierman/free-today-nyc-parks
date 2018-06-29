ba2c78a58e9b1549b625dd4d676e174736c41142
emanuelbierman <emanuel.bierman@gmail.com>
2018-06-29 13:54:20 -0400

    Add working scraper for descriptions. Continue working on scraper for start_time

--- lib/free_today_nyc_parks/scraper.rb
+++ lib/free_today_nyc_parks/scraper.rb
@@ -12,16 +12,26 @@ class FreeTodayNycParks::Scraper
       today = doc.css("div#events_leftcol h2").text
       FreeTodayNycParks::Event.today=(today)
 
-      locations = []
-      doc.css("div#events_leftcol div h4.location span").each {|e| locations << e.text unless e.text.start_with?(",") }
+      descriptions = []
+      doc.css("div#events_leftcol div span.description").each {|s| descriptions << s.text }
 
-      # locations.each_with_index do |loc|
-      #
-      # end
+      locations = []
+      doc.css("div#events_leftcol div h4.location span").each {|s| locations << s.text unless s.text.start_with?(",") }
 
       FreeTodayNycParks::Event.all.each_with_index do |event, index|
         event.location = locations[index]
       end
+
+      # start_times = []
+      # @doc.css("div#events_leftcol div").each do |d|
+      #   start_times << d.css("p strong")[0]
+      # end
+      # start_times.select {|e| e.class == String }
+      # ==> ["9:00 a.m."]
+
+      # doc.css("div#events_leftcol div")[0].css("p strong")[0].text
+      # ==> "9:00 a.m."
+
 
   end
 
